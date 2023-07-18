992ab94c2847658521286c2a443617763a73df59
Aleksey <pavlinov24121990@gmail.com>
2023-07-18 16:52:46 +0200

    Finish_views_users_and_functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+  validates :name, presence: true, length: {minimum: 2, maximum: 20}
+  validates :surname, presence: true, length: {minimum: 2, maximum: 20}
 end
