a55139f2123b80020dcdcccaae076b718c9023d6
Aleksey <pavlinov24121990@gmail.com>
2023-07-20 12:00:29 +0200

    Functionality_and_views_comments_V2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true, length: {minimum: 5, maximum: 100}
+
+  scope :approved, -> { where(approved: :approved) }
+  
 end
